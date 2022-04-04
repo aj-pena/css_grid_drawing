d9b575e653d125fd1d3fba1e71df464c3248331c
Andres Pena <penchjpc@gmail.com>
2022-04-04 12:58:09 -0400

    initial draft

---
+++ index.js
@@ -0,0 +1,38 @@
+// const grid = document.getElementById('grid')
+// let show = false
+// const gridArr = []
+
+// // Draw the grid
+// for (let i = 1; i < 1751; i++){
+//    const divEl = document.createElement('div')
+//    divEl.classList.add('grid-square')
+//    divEl.id = i
+//    grid.appendChild(divEl)
+//    gridArr.push(divEl)
+
+// }
+// console.log(gridArr)
+// // color the head square
+// for (let i = 362; i < 1751; i++){
+ 
+//  }
+ 
+
+// //  for (let i = 1; i < 1751; i++){
+ 
+// // }
+
+
+// // show / hide number of square on click
+// function showNumber(e){
+//     show = !show
+//     if(show){
+//         e.target.innerText = e.target.id
+//     }else {
+//         e.target.innerText = ""
+//     }
+    
+// }
+
+// // show / hide number on click event listener
+// grid.addEventListener('click', showNumber)
